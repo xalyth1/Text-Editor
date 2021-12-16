e2e788a47d95b357263bf0f22d868b396462f0b5
xlyth1 <xalyth10@gmail.com>
2021-12-16 18:20:57 +0100

    initial commit

---
+++ src/editor/TextEditor.java
@@ -0,0 +1,12 @@
+package editor;
+
+import javax.swing.*;
+
+public class TextEditor extends JFrame {
+    public TextEditor() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+        setLayout(null);
+    }
+}
